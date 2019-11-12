f8c89ecb17eb6ec2e425cd0273c79c6f9c737275
ruge23 <ruizgerezr@gmail.com>
2019-11-12 14:01:14 -0300

    callejeritos

--- src/pages/perfil-callejerito/perfil-callejerito.ts
+++ src/pages/perfil-callejerito/perfil-callejerito.ts
@@ -1,24 +1,86 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { ApiProvider } from './../../providers/api/api';
+import { Storage } from '@ionic/storage';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Slides, ToastController, ModalController } from 'ionic-angular';
+import { ModalCallejeritoPage } from '../modal-callejerito/modal-callejerito';
 
-/**
- * Generated class for the PerfilCallejeritoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-perfil-callejerito',
   templateUrl: 'perfil-callejerito.html',
 })
 export class PerfilCallejeritoPage {
+  dog: any;
+  @ViewChild('slideWithNav2') slideWithNav2: Slides;
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public toastController: ToastController,
+    public modalCtrl: ModalController,
+    private api: ApiProvider,
+    private storage: Storage
+  ) {
+    this.dog = this.navParams.data.dogDetail;
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  next() {
+    this.slideWithNav2.slideNext(500);
+  }
+
+  prev() {
+    this.slideWithNav2.slidePrev(500);
+  }
+
+  //EstadoDog
+  estado(dog) {
+    if (dog.estado == 2) {
+      return 'Perdido';
+    } else if (dog.estado == 3) {
+      return 'Encontrado'
+    } else if (dog.estado == 4) {
+      return 'En Adopción'
+    }else if (dog.estado == 5) {
+      return 'Callejerito'
+    } else {
+      return 'En Casa';
+    }
+  }
+
+  openModal(dog, i) {
+    const profileModal = this.modalCtrl.create(ModalCallejeritoPage, { dog: dog, index: i });
+    profileModal.onDidDismiss(data => {
+      console.log(data);
+    });
+    profileModal.present();
+  }
+
+  pedirTenencia(dog){
+    this.storage.get('datauser').then(val=>{
+      if(val!=null){
+        this.api.pedirTenenciaCalle(val['usuarioid'], dog.perroid).subscribe(x=>{
+          console.log('vueltaTenencia', x);
+          let vuelta = JSON.parse(x['_body'])['data'];
+          if(vuelta == 'inserted'){
+            this.tenencia(this.dog);
+          }
+        })
+      }
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilCallejeritoPage');
+  }
+
+  async tenencia(dog) {
+    const toast = await this.toastController.create({
+      message: "Listo!\n Nos encargaremos de avisarle\n a"+dog.usuarionombre+" que\n deseas tener la tenencia también\n de"+dog.nombre,
+      duration: 3000,
+      showCloseButton: true,
+      position: 'top',
+      closeButtonText: 'x'
+    });
+    toast.present();
   }
 
 }
