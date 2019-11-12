f496125090d7d167c393f611bb2d3ac1e4330e35
ruge23 <ruizgerezr@gmail.com>
2019-11-12 15:27:28 -0300

    test callejerito

--- src/pages/perfil-callejerito/perfil-callejerito.ts
+++ src/pages/perfil-callejerito/perfil-callejerito.ts
@@ -1,8 +1,10 @@
 import { ApiProvider } from './../../providers/api/api';
 import { Storage } from '@ionic/storage';
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams, Slides, ToastController, ModalController } from 'ionic-angular';
+import { NavController, NavParams, Slides, ToastController, ModalController, AlertController } from 'ionic-angular';
 import { ModalCallejeritoPage } from '../modal-callejerito/modal-callejerito';
+import { MyDogsPage } from '../my-dogs/my-dogs';
+import { BuscarUsuariosPage } from '../buscar-usuarios/buscar-usuarios';
 
 
 @Component({
@@ -10,6 +12,7 @@ import { ModalCallejeritoPage } from '../modal-callejerito/modal-callejerito';
   templateUrl: 'perfil-callejerito.html',
 })
 export class PerfilCallejeritoPage {
+  index:any;
   dog: any;
   @ViewChild('slideWithNav2') slideWithNav2: Slides;
   constructor(
@@ -17,10 +20,12 @@ export class PerfilCallejeritoPage {
     public navParams: NavParams,
     public toastController: ToastController,
     public modalCtrl: ModalController,
+    public alertCtrl: AlertController,
     private api: ApiProvider,
     private storage: Storage
   ) {
     this.dog = this.navParams.data.dogDetail;
+    this.index = this.navParams.get('index') ? this.navParams.get('index') : 0;
   }
 
   next() {
@@ -70,6 +75,26 @@ export class PerfilCallejeritoPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilCallejeritoPage');
+  }
+
+  addTenencia(dog) {
+    const alert = this.alertCtrl.create({
+      title: 'Compartir tenencia!',
+      subTitle: 'Estas seguro de compartir la tenecia de ' + dog.nombre + '?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'Cancel'
+        },
+        {
+          text: 'Si',
+          handler: data => {
+            this.navCtrl.push(BuscarUsuariosPage, { dogTransfer: dog, index: 1 })
+          }
+        }
+      ]
+    })
+    alert.present();
   }
 
   async tenencia(dog) {
